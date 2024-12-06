3cb84351836f71c9872d27e8453e9650ca8adf98
DevJaco <dev2708dev@gmail.com>
2024-12-07 01:53:47 +0200

    Linking truck stops to cities with a foreign key & adding canadian states

--- truck_routing/routing/models.py
+++ truck_routing/routing/models.py
@@ -88,31 +88,43 @@ class Route(models.Model):
     feature_collection = models.ForeignKey(
         FeatureCollection, on_delete=models.CASCADE)
 
+STATE_CHOICES = (
+    (1, 'AL'), (2, 'AK'), (3, 'AZ'), (4, 'AR'), (5, 'CA'),
+    (6, 'CO'), (7, 'CT'), (8, 'DE'), (9, 'FL'), (10, 'GA'),
+    (11, 'HI'), (12, 'ID'), (13, 'IL'), (14, 'IN'), (15, 'IA'),
+    (16, 'KS'), (17, 'KY'), (18, 'LA'), (19, 'ME'), (20, 'MD'),
+    (21, 'MA'), (22, 'MI'), (23, 'MN'), (24, 'MS'), (25, 'MO'),
+    (26, 'MT'), (27, 'NE'), (28, 'NV'), (29, 'NH'), (30, 'NJ'),
+    (31, 'NM'), (32, 'NY'), (33, 'NC'), (34, 'ND'), (35, 'OH'),
+    (36, 'OK'), (37, 'OR'), (38, 'PA'), (39, 'RI'), (40, 'SC'),
+    (41, 'SD'), (42, 'TN'), (43, 'TX'), (44, 'UT'), (45, 'VT'),
+    (46, 'VA'), (47, 'WA'), (48, 'WV'), (49, 'WI'), (50, 'WY'),
+    
+    (51, 'AB'), (52, 'BC'), (53, 'MB'), (54, 'NB'), (55, 'NL'),
+    (56, 'NS'), (57, 'ON'), (58, 'PE'), (59, 'QC'), (60, 'SK'), (61, 'YT')
+)
+
+class City(models.Model):
+    name = models.CharField(max_length=255)
+    state = models.IntegerField(choices=STATE_CHOICES)
+
+    location = models.PointField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.name}, {self.state}'
 
 class TruckStop(models.Model):
 
-    STATE_CHOICES = (
-        (1, 'AL'), (2, 'AK'), (3, 'AZ'), (4, 'AR'), (5, 'CA'),
-        (6, 'CO'), (7, 'CT'), (8, 'DE'), (9, 'FL'), (10, 'GA'),
-        (11, 'HI'), (12, 'ID'), (13, 'IL'), (14, 'IN'), (15, 'IA'),
-        (16, 'KS'), (17, 'KY'), (18, 'LA'), (19, 'ME'), (20, 'MD'),
-        (21, 'MA'), (22, 'MI'), (23, 'MN'), (24, 'MS'), (25, 'MO'),
-        (26, 'MT'), (27, 'NE'), (28, 'NV'), (29, 'NH'), (30, 'NJ'),
-        (31, 'NM'), (32, 'NY'), (33, 'NC'), (34, 'ND'), (35, 'OH'),
-        (36, 'OK'), (37, 'OR'), (38, 'PA'), (39, 'RI'), (40, 'SC'),
-        (41, 'SD'), (42, 'TN'), (43, 'TX'), (44, 'UT'), (45, 'VT'),
-        (46, 'VA'), (47, 'WA'), (48, 'WV'), (49, 'WI'), (50, 'WY')
-    )
-
+    
     opis_id = models.IntegerField()
     name = models.CharField(max_length=1023)
     address = models.CharField(max_length=1023)
-    city = models.CharField(max_length=1023)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
     state = models.SmallIntegerField(choices=STATE_CHOICES)
     #  use an integer field here as we will price in cents for accuracy.
     fuel_retail_price = models.IntegerField(help_text="Truck Stop fuel price in cents. (USD)")
-    #  always want to use coordinates in our system.
-    coordinate = models.PointField()
+    coordinate = models.PointField(blank=True, null=True)
 
     def __str__(self) -> str:
         return f'{self.name}, {self.address}, {self.city}, {self.state}'
+    
