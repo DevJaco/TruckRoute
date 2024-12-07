6175dc02ff1aea47cb1b6be76db702eb6d72d742
DevJaco <dev2708dev@gmail.com>
2024-12-07 08:43:42 +0200

    Updating admin to match new model file, updating fuel_price to use a decimal field

--- truck_routing/routing/admin.py
+++ truck_routing/routing/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Route, BoundingBox, Feature, FeatureCollection, Metadata, Segment, Step, WayPoint, FeatureSummary
+from .models import Route, BoundingBox, Feature, FeatureCollection, Metadata, Segment, Step, WayPoint, FeatureSummary, City, TruckStop
 
 class FeatureInline(admin.StackedInline):
     model = Feature
@@ -9,12 +9,24 @@ class FeatureInline(admin.StackedInline):
 class FeatureCollectionAdmin(admin.ModelAdmin):
     inlines = [FeatureInline]
 
+class StepAdmin(admin.ModelAdmin):
+    filter_horizontal = ['way_points']
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ['name', 'state_label_display']
+    list_filter = ['state']
+
+    def state_label_display(self, obj):
+        return obj.get_state_display()
+
 admin.site.register(Route)
 admin.site.register(BoundingBox)
 admin.site.register(Feature)
 admin.site.register(FeatureCollection, FeatureCollectionAdmin)
 admin.site.register(Metadata)
 admin.site.register(Segment)
-admin.site.register(Step)
+admin.site.register(Step, StepAdmin)
 admin.site.register(WayPoint)
 admin.site.register(FeatureSummary)
+admin.site.register(City, CityAdmin)
+admin.site.register(TruckStop)
